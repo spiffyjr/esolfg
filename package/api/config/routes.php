f0a32ca6a3258cf47fb3070ff8f04a8417cb6d6d
Kyle Spraggs <theman@spiffyjr.me>
2014-06-08 20:35:27 -0500

    Updated composer.json

--- package/api/config/routes.php
+++ package/api/config/routes.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'api.groups.get' => ['/api/groups', 'Api\Group\GroupGetAction', ['methods' => ['get']]],
+    'api.groups.get' => ['/api/groups', 'Api\Group\GroupGetAction'],
     'api.groups.post' => ['/api/groups', 'Api\Group\GroupPostAction', ['methods' => ['post']]],
-    'api.events' => ['/api/events', 'Api\Event\EventGetAction', ['methods' => ['get']]],
-    'api.events/types' => ['/api/events/{id:\d+}/types', 'Api\Event\EventTypeGetAction', ['methods' => ['get']]],
-    'api.servers' => ['/api/servers', 'Api\Server\ServerGetAction', ['methods' => ['get']]]
+    'api.events' => ['/api/events', 'Api\Event\EventGetAction'],
+    'api.events/types' => ['/api/events/{id:\d+}/types', 'Api\Event\EventTypeGetAction'],
+    'api.servers' => ['/api/servers', 'Api\Server\ServerGetAction']
 ];
